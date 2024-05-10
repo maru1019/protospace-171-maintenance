5e99e694da4e8f7c9dcae051c210cef057accc65
ito-mirai <tetudounohi@gmail.com>
2024-05-10 16:09:25 +0900

    userモデルの外部キーの追加結果

--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_09_092656) do
     t.text "concept", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-ActiveRecord::Schema[7.0].define(version: 2024_05_09_084930) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_09_084930) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
