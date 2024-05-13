107882b300f3559dc32327f8bc4a0bffd191a0e7
unagianago <fumiyakm4580@gmail.com>
2024-05-13 11:14:20 +0900

    ユーザー名にユーザー詳細ページへのパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
   resources :prototypes, only: [:index, :create, :new, :show]
-  resources :users, only: [:show]
+  resources :users, only: :show
 end
